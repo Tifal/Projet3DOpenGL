b1ebf6f560262552219c457058bd3811f6610244
Raphaël Fitoussi <fitoussi.raph@gmail.com>
2016-06-21 21:15:20 +0200

    doc filewindow + uml

--- filewindow.h
+++ filewindow.h
@@ -15,12 +15,31 @@
 #include <QStringList>
 #include <QTableWidgetItem>
 #include <QSizePolicy>
+
+/**
+ * @brief The FileWindow class
+ * FileWindow is a class that enables two display possibilities for coordinates in a table.
+ */
 class FileWindow : public QWidget
 {
 
 public:
+    /**
+     * @brief FileWindow
+     * Constructor that creates an instance of a FileWindow
+     */
     FileWindow(QWidget *parent = Q_NULLPTR);
+
+    /**
+     * @brief setViewMrHolt
+     * Sets the window view to a table with the 3 coordinates in one same cell, the timesteps in rows and the markers in columns.
+     */
     void setViewMrHolt();
+
+    /**
+     * @brief setViewMrNilsen
+     * Sets the window view to a table with one coordinate in one cell, the timesteps in columns and the markers in rows.
+     */
     void setViewMrNilsen();
 };
 
